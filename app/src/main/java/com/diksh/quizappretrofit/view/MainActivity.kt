1463d481cc91fe4c7cc1eef1d1da856ee751c556
Diksh Sawaliya <80209884+dikshsawaliya@users.noreply.github.com>
2024-11-25 23:39:39 +0530

    Updated the main activity with data binding

--- app/src/main/java/com/diksh/quizappretrofit/view/MainActivity.kt
+++ app/src/main/java/com/diksh/quizappretrofit/view/MainActivity.kt
@@ -1,20 +1,43 @@
-package com.diksh.quizappretrofit
+package com.diksh.quizappretrofit.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.diksh.quizappretrofit.R
+import com.diksh.quizappretrofit.databinding.ActivityMainBinding
+import com.diksh.quizappretrofit.model.Question
+import com.diksh.quizappretrofit.model.QuestionList
+import com.diksh.quizappretrofit.viewmodel.QuizViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizViewModel: QuizViewModel
+    lateinit var questionList: List<Question>
+
+
+    //Whenever you start any quiz, we need to instantiate a result and total questions
+    companion object{
+        var result = 0
+        var totalQuestion = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //Since we are going to use this app many times, we need to reset the quiz (scores = 0, totalQuestions = 0)
+        result = 0
+        totalQuestion = 0
+
+        quizViewModel = ViewModelProvider(this)
+                        .get(QuizViewModel::class.java)
+
+
     }
 }
